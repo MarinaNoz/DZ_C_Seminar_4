83acf0238718d9ccac4ec50b2b86f4f8b3ea149d
Marina <troskom@mail.ru>
2022-04-28 15:23:01 +0300

    DZ_Seminar_4

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,11 +1,22 @@
 ﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
 
 int[]numbers = new int[8];
-string mass = String.Empty;
-for (int i = 0; i < numbers.Length; i++)
+FillArray(numbers);
+WriteArray(numbers);
+
+void FillArray(int[] numbers)
 {
-Console.Write($"Введите число {i+1}: ");
-numbers[i] = Convert.ToInt32(Console.ReadLine());
-mass = mass + numbers[i];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
 }
-Console.Write(mass);
+void WriteArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
